2e837abe8c6569a5ffb46d7f10eb2a62fece5245
Alexandros Tsikakis <Alexandros.Tsikakis@ymail.com>
2023-02-20 14:25:10 +0100

    added search location function and results.php

--- header.php
+++ header.php
@@ -17,20 +17,24 @@ require 'includes/env.php';
                 <h1>UFO Sighting</h1>
                 <h3>Submit your UFO sighting</h3>
                 <nav class="navbar navbar-expand-lg bg-light">
-                <a class="navbar-brand" href="#">Navbar</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                    <a class="nav-link" href="/">Home</a>
-                    <a class="nav-link" href="aboutus.php">About us</a>
-                    <a class="nav-link" href="blog.php">Blog</a>
-                    <a class="nav-link" href="history.php">Sighting list</a>
-                    <a class="nav-link" href="contact.php">Contact</a>
-                </div>
-                </div>
-            </nav>
+                  <a class="navbar-brand" href="#">Navbar</a>
+                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                  </button>
+                  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                      <a class="nav-link" href="/">Home</a>
+                      <a class="nav-link" href="aboutus.php">About us</a>
+                      <a class="nav-link" href="blog.php">Blog</a>
+                      <a class="nav-link" href="history.php">Sighting list</a>
+                      <a class="nav-link" href="contact.php">Contact</a>
+                    </div>
+                    <form class="d-flex" action="results.php" method="get">
+                      <input class="form-control me-2" type="search" placeholder="Search location" aria-label="Search" name="search">
+                      <button class="btn btn-outline-primary" type="submit">Search</button>
+                    </form>
+                  </div>
+                </nav>
     </div>
 
     <div class="container">
