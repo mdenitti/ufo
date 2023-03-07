895ab48c15bd1b4d3bc8d0784d9414328e5ad36b
Zuzanna <zahorska.z96@gmail.com>
2023-03-07 11:22:39 +0100

    add basic styling

--- header.php
+++ header.php
@@ -5,40 +5,53 @@ require 'includes/env.php';
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <title>ufo</title>
-    <link rel="icon" type="image/x-icon" href="./assets/favicon.ico" />
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
-      integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
-      crossorigin="anonymous" referrerpolicy="no-referrer" />  
-</head>
-  <body>
-    <div class="container">
-                <h1>UFO Sighting</h1>
-                <h3>Submit your UFO sighting</h3>
-                <nav class="navbar navbar-expand-lg bg-light">
-                  <a class="navbar-brand" href="#">Navbar</a>
-                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                  </button>
-                  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div class="navbar-nav">
-                      <a class="nav-link" href="index.php">Home</a>
-                      <a class="nav-link" href="aboutus.php">About us</a>
-                      <a class="nav-link" href="blog.php">Blog</a>
-                      <a class="nav-link" href="history.php">Sighting list</a>
-                      <a class="nav-link" href="contact.php">Contact</a>
-                    </div>
-                    <form class="d-flex" action="results.php" method="get">
-                      <input class="form-control me-2" type="search" placeholder="Search location" aria-label="Search" name="search">
-                      <button class="btn btn-outline-primary" type="submit">Search</button>
-                    </form>
-                  </div>
-                </nav>
-    </div>
 
-    <div class="container">
+<head>
+  <title>ufo</title>
+  <link rel="icon" type="image/x-icon" href="./assets/favicon.ico" />
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <!-- Google Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
+  <!-- Bootstrap -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+    integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  <div class="container">
+
+    <nav class="navbar navbar-expand-lg  ">
+      <a class="navbar-brand" href="#"><img class="logo" src="assets/images/SPACESPY.png"></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-link" href="index.php">Home</a>
+          <a class="nav-link" href="aboutus.php">About us</a>
+          <a class="nav-link" href="blog.php">Blog</a>
+          <a class="nav-link" href="history.php">Sighting list</a>
+          <a class="nav-link" href="contact.php">Contact</a>
+        </div>
+        <!-- <div class="search">
+          <form class="d-flex" action="results.php" method="get">
+            <input class="form-control me-2" type="search" placeholder="Search location" aria-label="Search"
+              name="search">
+            <button class="btn btn-outline-primary" type="submit">Search</button>
+          </form>
+        </div> -->
+
+      </div>
+    </nav>
+  </div>
+
+  <div class="container">
--- index.php
+++ index.php
@@ -1,27 +1,49 @@
 <?php require 'header.php' ?>
 
-        <div class="row">
-            <div class="col">
-        
-                <h6>Fill out the form below to submit your UFO sighting
-                - <?php echo getDateInDutch()?>
-                </h6>
+<div class="row">
+    <div class="col">
 
-                <form method="post" action="process.php" enctype="multipart/form-data" class="bg-dark p-2 text-dark bg-opacity-10 rounded mb-2">
-                    <input class='form-control' type="text" name="name" placeholder="Name"><hr>
-                    <input class='form-control' type="email" name="email" placeholder="Email"><hr>
-                    <input class='form-control' type="text" name="location" placeholder="Location"><hr>
-                    <input class='form-control' type="date" name="date" placeholder="Date"><hr>
-                    <input class='form-control' type="time" name="time" placeholder="Time"><hr>
-                    <div class="form-check">
-                      <input class="form-check-input" type="checkbox" name="scary">
-                          Does the alien look scary?
-                    </div>
-                    <textarea class='form-control' name="message"></textarea><br>
-                    <input class="form-control" type="file" name="alienImg"><hr>
-                    <input class='btn btn-primary' type="submit" value="Submit">
-                </form>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-img">
+                    <img src="assets/images/Taken.png" alt="taken">
+                </div>
+                <div class="col-lg-6 col-form">
+                    <h6>Fill out the form below to submit your UFO sighting
+                        -
+                        <?php echo getDateInDutch() ?>
+                    </h6>
+                    <form method="post" action="process.php" enctype="multipart/form-data"
+                        class=" input-form rounded mb-2">
+                        <input class='form-control form-input-field' type="text" name="name" placeholder="Name">
+
+                        <input class='form-control form-input-field' type="email" name="email" placeholder="Email">
+
+                        <input class='form-control form-input-field' type="text" name="location" placeholder="Location">
+
+                        <input class='form-control form-input-field' type="date" name="date" placeholder="Date">
+
+                        <input class='form-control form-input-field' type="time" name="time" placeholder="Time">
+
+                        <div class="form-check form-input-field">
+                            <input class="form-check-input " type="checkbox" name="scary">
+                            Does the alien look scary?
+                        </div>
+                        <textarea class='form-control form-input-field' name="message"
+                            placeholder="Describe your encounter!"></textarea><br>
+                        <input class="form-control form-input-field" type="file" name="alienImg">
+                        <hr>
+                        <div class="submit">
+                            <input class='btn btn-primary submit-button' type="submit" value="Submit">
+
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
 
- <?php require 'footer.php' ?>
+    </div>
+</div>
+
+<?php require 'footer.php' ?>
